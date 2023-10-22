058fb5e90f7d280df67293153d3f23f6e30a600d
kennethmori <110390702+kennethmori@users.noreply.github.com>
2023-10-22 20:56:20 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using LaboratoryExceptionHandling;
+using System.ComponentModel;
+using System.Data.SqlTypes;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LabExceptionHandling;
+
+class Program
+{
+    //exercise 2 method
+    private static void ReadNumber(int start, int end)
+    {
+        try
+        {
+            int min = 0;
+            int max = 9;
+
+            while (min <= max)
+            {
+                {
+                    try
+                    {
+
+                        min++;
+                        Console.WriteLine($"Enter number {min}:");
+                        int num = int.Parse(Console.ReadLine());
+                        if (num < start || num > end)
+                        {
+                            throw new myException($"{num} is invalid.");
+                        }
+                    }
+                    catch (myException ex)
+                    {
+                        Console.WriteLine($"{ex.Message} Number must be between Start and End.");
+                    }
+                }
+            }
+            Console.WriteLine("End of loop body.");
+        }
+
+        catch (FormatException ex)
+        {
+            Console.WriteLine($"Invalid number or non-number text is entered {ex}");
+        }
+    }
+
+    //main 
+    static void Main(string[] args)
+    {
+        //1.
+        try
+        {
+            Console.WriteLine("\n------------------------------Exercise 1------------------------------");
+            Console.WriteLine("Enter an integer:");
+            double input = int.Parse(Console.ReadLine());
+            if (input < 0)
+            {
+                throw new FormatException();
+            }
+
+            double calculates = Math.Sqrt(input);
+            Console.WriteLine($"The square root of {input} is: {calculates}");
+        }
+
+        catch (FormatException ex)
+        {
+            Console.WriteLine($"Invalid. Must be a positive integer. {ex.Message}");
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+
+        finally
+        {
+            Console.WriteLine("Goodbye");
+        }
+
+
+        //2.
+        Console.WriteLine("\n------------------------------Exercise 2------------------------------");
+        Console.WriteLine("Enter Start:");
+        int Start = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter End:");
+        int End = int.Parse(Console.ReadLine());
+
+        ReadNumber(Start, End);
+    }
+}
